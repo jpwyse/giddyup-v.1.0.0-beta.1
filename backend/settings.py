d52161ac4d7a7ea4cdacaf9bbf1a73ff56ca8f2a
James Wyse <jpwyse@gmail.com>
2023-03-14 21:42:42 -0400

    pre-deploy commit edit

--- backend/settings.py
+++ backend/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Email Settings 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
